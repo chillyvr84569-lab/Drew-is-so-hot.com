47d3a2067fe0574e9132304db355ab9c278a0d7e
chillyvr84569-lab <chillyvr84569@gmail.com>
2026-01-29 20:58:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+// ... existing code ...
 card.onclick = () => {
-    if (item.type === "new") {
-        // This opens a 'blank' window first so Securly doesn't see your site as the 'Referrer'
-        const win = window.open();
-        win.opener = null; 
-        win.location = item.url;
-    } else {
-        // ... standard iframe code
-    }
+    // Decode the URL on the fly to hide it from Securly's initial scan
+    const decodedUrl = atob(item.url); 
+    
+    // Success hack: Open as a "blank" window to strip the referrer
+    const win = window.open('about:blank', '_blank');
+    win.document.write(`
+        <title>Loading Resources...</title>
+        <script>window.location.replace("${decodedUrl}#translate.google.com");</script>
+    `);
+    win.opener = null; 
 };
